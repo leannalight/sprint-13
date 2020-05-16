ccd60ad7fdf0f8d8c70a69f9f170837040db9f6b
Anna Lee <annaleeart@yandex.ru>
2020-05-16 12:55:18 +0300

    Connected to MongoDB server in app.js. Initialized mongoose, mongod

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
 
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
@@ -7,6 +8,11 @@ const cardsRouter = require('./routes/cards');
 const app = express();
 const { PORT = 3000 } = process.env;
 
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', usersRouter);
