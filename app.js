ef42614f4158e690dbcd9b96026ae1c4d3bf66ef
Anna Lee <annaleeart@yandex.ru>
2020-05-18 22:01:23 +0300

    Authorization temporary solution

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
 const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const { PORT = 3000 } = process.env;
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
+})
+  // eslint-disable-next-line no-console
+  .then(() => console.log('MongoDB has started ...'))
+  // eslint-disable-next-line no-console
+  .catch((error) => console.log(error));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '5d8b8592978f8bd833ca8133', // вставьте сюда _id созданного в предыдущем пункте пользователя
+  };
+
+  next();
 });
 
 app.use('/', require('./routes/users'));
-
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', require('./routes/cards'));
 
 app.all('*', (req, res) => {
   res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
