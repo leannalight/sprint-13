4b6aa82d59280f82fe2bc9f626b5b4033e44edb1
Anna Lee <annaleeart@yandex.ru>
2020-05-21 18:07:47 +0300

    Review 1: added error handling and res.status 400, res.status.404

--- controllers/cards.js
+++ controllers/cards.js
@@ -3,7 +3,12 @@ const Card = require('../models/card');
 module.exports.getCards = (req, res) => {
   Card.find({})
     .populate('owner')
-    .then((cards) => res.send({ data: cards }))
+    .then((cards) => {
+      if (cards.length === 0) {
+        return res.status(404).send({ message: 'Карточки отсутствуют в списке' });
+      }
+      return res.send({ data: cards });
+    })
     .catch((error) => res.status(500).send({ message: error.message }));
 };
 
@@ -12,17 +17,33 @@ module.exports.createCard = (req, res) => {
 
   Card.create({ name, link, owner: req.user._id })
     .then((card) => res.send({ data: card }))
-    .catch((error) => res.status(500).send({ message: error.message }));
+    .catch((error) => {
+      if (error.name === error.ValidationError) {
+        return res.status(400).send({ message: error.message });
+      }
+      if (error.name === error.CastError) {
+        return res.status(400).send({ message: error.message });
+      }
+      return res.status(500).send({ message: error.message });
+    });
 };
 
 module.exports.deleteCardbyId = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
     .then((card) => {
-      if (card !== null) {
-        res.send({ message: 'Карточка успешно удалена' });
-      } else {
-        res.status(404).send({ message: 'Нет карточки с таким id' });
+      const cardFind = card.find((items) => items.id === req.params.cardId);
+      if (!cardFind) {
+        return res.status(404).send({ message: 'Нет карточки с таким id' });
       }
+      return res.send({ data: cardFind });
     })
-    .catch((error) => res.status(500).send({ message: error.message }));
+    .catch((error) => {
+      if (error.name === error.ValidationError) {
+        return res.status(400).send({ message: error.message });
+      }
+      if (error.name === error.CastError) {
+        return res.status(400).send({ message: error.message });
+      }
+      return res.status(500).send({ message: error.message });
+    });
 };
